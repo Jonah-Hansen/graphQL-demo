88f8c6b0be0ed06773745bc74a245ac1fbe57ed1
Jonah-Hansen <tce_mc@telus.net>
2023-07-03 18:20:50 -0600

    add resolver for updating view count

--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -9,6 +9,29 @@ const resolvers = {
       return dataSources.trackAPI.getTrack(id)
     }
   },
+
+  Mutation: {
+    // increments a track's numberOfViews property
+    incrementTrackViews: async (_parent, { id }, { dataSources }, _info) => {
+      try {
+        const track = await dataSources.trackAPI.incrementTrackViews(id)
+        return {
+          code: 200,
+          success: true,
+          message: `Successfully incremented number of views for track ${id}`,
+          track,
+        }
+      } catch (error) {
+        return {
+          code: error.extensions.response.status,
+          success: false,
+          message: error.extensions.response.body,
+          track: null
+        }
+      }
+    }
+  },
+
   Track: {
     author: ({ authorId }, _args, { dataSources }, _info) => {
       return dataSources.trackAPI.getAuthor(authorId)
@@ -16,7 +39,7 @@ const resolvers = {
     modules: ({ id }, _args, { dataSources }, _info) => {
       return dataSources.trackAPI.getModules(id)
     }
-  }
+  },
 }
 
 module.exports = resolvers
